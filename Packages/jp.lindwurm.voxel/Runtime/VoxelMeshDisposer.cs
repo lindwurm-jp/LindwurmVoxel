1de26a9c8aee7c1859d45d43623295c08299b992
NoF <nof@lindwurm.jp>
2025-09-19 17:33:20 +0900

    v 1.0.4 : ボクセルのGameObjectをInstantiateしたのをDestroyしたときにsharedMeshが削除するのを最後の一つで削除するように修正

--- Packages/jp.lindwurm.voxel/Runtime/VoxelMeshDisposer.cs
+++ Packages/jp.lindwurm.voxel/Runtime/VoxelMeshDisposer.cs
@@ -1,15 +1,87 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Lindwurm.Voxel
 {
     public class VoxelMeshDisposer : MonoBehaviour
     {
-        public Mesh targetMesh = null;
+        static Dictionary<Mesh, int> meshRefCount = new Dictionary<Mesh, int>();
+
+        [HideInInspector]
+        public Mesh mesh = null;
+        public Mesh targetMesh {
+            get
+            {
+                return mesh;
+            }
+            set
+            {
+                //Debug.Log("targetMesh Add");
+                var m = value;
+                if(mesh == m)
+                {
+                    return;
+                }
+                if (mesh != null && meshRefCount.ContainsKey(mesh))
+                {
+                    meshRefCount[mesh]--;
+                    if (meshRefCount[mesh] <= 0)
+                    {
+                        meshRefCount.Remove(mesh);
+                        Destroy(mesh);
+                    }
+                }
+                if(meshRefCount.ContainsKey(m))
+                {
+                    meshRefCount[m]++;
+                }else
+                {
+                    meshRefCount[m] = 1;
+                }
+                mesh = m;
+            }
+        }
+
+        private void Awake()
+        {
+            //Debug.Log($"new Disposer Awake {gameObject.name},{targetMesh==null}");
+            if(mesh != null)
+            {
+                var mf = GetComponent<MeshFilter>();
+                if (mf != null && mf.sharedMesh != mesh)
+                {
+                    // instantiateÇµÇΩéûì_Ç≈ä«óùÇ≥ÇÍÇƒÇ¢ÇÈmeshÇ∆àŸÇ»Ç¡ÇΩÇÁñ{êlÇ™ä«óùÇ∑ÇÈ
+                    mesh = null;
+                    return;
+                }
+                if (meshRefCount.ContainsKey(mesh))
+                {
+                    meshRefCount[mesh]++;
+                }
+                else
+                {
+                    meshRefCount[mesh] = 1;
+                }
+                //Debug.Log($"Count:[{mesh.GetHashCode()}] {meshRefCount[mesh]}");
+            }
+        }
+
+
         private void OnDestroy()
         {
-            if (targetMesh != null)
+            if (mesh != null && meshRefCount.ContainsKey(mesh))
             {
-                Destroy(targetMesh);
+                meshRefCount[mesh]--;
+                //Debug.Log(meshRefCount[targetMesh]);
+                if (meshRefCount[mesh] <= 0)
+                {
+                    meshRefCount.Remove(mesh);
+                    Destroy(mesh);
+                }
+            }else
+            {
+                //Debug.Log("Unknown mesh");
             }
         }
     }
